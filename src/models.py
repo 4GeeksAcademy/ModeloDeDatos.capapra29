3599edce2d6241e613a2c35f7df48f6cf6e4b3bd
capapra29 <capapra29@gmail.com>
2024-11-18 19:29:31 +0000

    modelado

--- src/models.py
+++ src/models.py
@@ -13,6 +13,8 @@ class Person(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
 
 class Address(Base):
     __tablename__ = 'address'
@@ -27,6 +29,37 @@ class Address(Base):
 
     def to_dict(self):
         return {}
+    
+class Personaje(Base):
+    __tablename__ = 'personaje'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    gender = Column(String(20), nullable=False)
+    eye_color = Column(String(20), nullable=False)
+
+class Planeta(Base):
+    __tablename__ = 'planeta'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    population = Column(Integer, nullable=False)
+    weather = Column(String(250), nullable=False)
+
+class Favorito(Base):
+    __tablename__ = 'favorito'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    person_id = Column(Integer, ForeignKey("person.id"))
+    person=relationship(Person)
+    planeta_id = Column(Integer, ForeignKey("planeta.id"))
+    planeta=relationship(Planeta)
+    personaje_id = Column(Integer, ForeignKey("personaje.id"))
+    personaje=relationship(Personaje)
+   
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
